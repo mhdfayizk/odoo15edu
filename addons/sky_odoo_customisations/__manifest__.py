5e66899f3993f696cb8d77bcbca2d6a53d4a3962
christy <christyabraham189@gmail.com>
2024-03-14 10:28:59 +0530

    New changes in id generation

--- addons/sky_odoo_customisations/__manifest__.py
+++ addons/sky_odoo_customisations/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
 	'name': "Sky Secretary Odoo Customisations",
-	'version': "15.0.0.1",
+	'version': "15.0.0.2",
 	'category': "Human Resources/Employees",
     'license':'OPL-1',
 	'summary': "This Module provides customisations for different modules",
--- addons/sky_odoo_customisations/models/hr_employee.py
+++ addons/sky_odoo_customisations/models/hr_employee.py
@@ -6,47 +6,66 @@ from odoo import api, fields, models
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    user_code = fields.Char(string="User ID",readonly="1")
+    user_code = fields.Char(string="User ID")
     user_code_autogenerate = fields.Char(string="Code Sequence") 
     
     
         
         
+    
+    
     @api.model
     def create(self, vals):
-        print("Hello")
         if not vals.get('user_code_autogenerate'):
             vals['user_code_autogenerate'] = self.env['ir.sequence'].next_by_code('employee.sequence')
     
         company_id = self.env['res.company'].browse(vals.get('company_id'))
         department_id = self.env['hr.department'].browse(vals.get('department_id'))
+    
+        user_code = ''
+    
         if company_id:
             company_code = company_id.company_code
+            user_code += company_code
+    
         if department_id:
-            department_code = department_id.dep_code  
-            if vals.get('user_code_autogenerate'):
-                vals['user_code'] = f"{company_code}{department_code}{vals['user_code_autogenerate']}"
-            else:
-                vals['user_code'] = company_code
+            department_code = department_id.dep_code
+            user_code += department_code
+    
+        if vals.get('user_code_autogenerate'):
+            user_code += vals['user_code_autogenerate']
+    
+        vals['user_code'] = user_code
     
         res = super(HrEmployee, self).create(vals)
-        return res  
-        
+        return res
+    
+    
     
     def write(self, vals):
-        company_code=''
-        department_code=''
-        if 'company_id' in vals:
-            new_company_id = self.env['res.company'].browse(vals.get('company_id'))
-            company_code = new_company_id.company_code
-            
-        if 'department_id' in vals:
-            new_department_id = self.env['hr.department'].browse(vals.get('department_id'))
-            department_code = new_department_id.dep_code    
-            
-            if self.user_code_autogenerate:
-                new_user_code = f"{company_code}{department_code}{self.user_code_autogenerate}"
-                vals['user_code'] = new_user_code
+        for rec in self:
+            if 'user_code_autogenerate' in vals:
+                user_code_autogenerate = vals['user_code_autogenerate']
+            else:
+                user_code_autogenerate = rec.user_code_autogenerate
+    
+            company_id = rec.company_id if 'company_id' not in vals else self.env['res.company'].browse(vals.get('company_id'))
+            department_id = rec.department_id if 'department_id' not in vals else self.env['hr.department'].browse(vals.get('department_id'))
+    
+            user_code = ''
+    
+            if company_id:
+                company_code = company_id.company_code
+                user_code += company_code
+    
+            if department_id:
+                department_code = department_id.dep_code
+                user_code += department_code
+    
+            if user_code_autogenerate:
+                user_code += user_code_autogenerate
+    
+            vals['user_code'] = user_code
     
         return super(HrEmployee, self).write(vals)
 
